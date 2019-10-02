075fd10f092d0645bb98e168bebc3fc7000b7185
Yaroslav Mamykin (GitHub) <ymamykin@luxoft.com>
2019-10-02 12:06:42 +0300

    Fix coverage script

--- tools/infrastructure/collect_coverage.sh
+++ tools/infrastructure/collect_coverage.sh
@@ -17,8 +17,8 @@ rm -rf $COVERAGE_DIR
 rm -rf $REPORTS_DIR -
 
 mkdir $COVERAGE_DIR
-lcov --quiet --capture --directory . --output-file $COVERAGE_DIR/full_report.info
-lcov --quiet --remove $COVERAGE_DIR/full_report.info '/usr/*' '*/test/*' '*/src/3rd*' '*/build/src/*'  --output-file $COVERAGE_DIR/coverage.info
+lcov --capture --directory ${BUILD_DIR} --output-file ${COVERAGE_DIR}/full_report.info
+lcov --remove $COVERAGE_DIR/full_report.info '/usr/*' '*/test/*' '*/src/3rd*' '*/build/src/*'  --output-file $COVERAGE_DIR/coverage.info
 
 mkdir $REPORTS_DIR
 genhtml --quiet $COVERAGE_DIR/coverage.info --output-directory $REPORTS_DIR
